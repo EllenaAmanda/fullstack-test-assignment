c4bd00ddfef2efe605ba8c816f8283ad04e3a588
EllenaAmanda <ellenaamanda846@gmail.com>
2025-03-06 15:22:31 +0700

    configured css and tailwind

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Home = () => {
   return (
     <div>
-      <div className="">
+      <div className="flex justify-between">
           <h1>Income Trip</h1>
           <button>Add Trip</button>
       </div>
