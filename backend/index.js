612f1e07e193962bb95e9740e808e58f68b4b9d4
EllenaAmanda <ellenaamanda846@gmail.com>
2025-03-11 13:06:36 +0700

    added delete controller

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const http = require("http");
+const cors = require("cors");
 
 const app = express();
 const server = http.createServer(app);
 
 const router = require("./src/routes");
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api", router);
--- backend/src/controller/trip.js
+++ backend/src/controller/trip.js
@@ -4,37 +4,41 @@ const TRIPS = [
     id: 0,
     title: "6D/4N Fun Tassie Vacation + Sydney",
     description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+      "Experience a 6-day, 4-night adventure exploring the stunning landscapes of Tasmania and the vibrant city of Sydney. Enjoy comfortable stays in a 4-star hotel, seamless travel with Qatar Airways, and delicious meals as per the itinerary. Embark on an unforgettable journey through Australia’s breathtaking scenery and cultural highlights.",
     image:
       "https://res.cloudinary.com/dmegegwqb/image/upload/v1638670749/trip-default-dewetour/sydney_p4d1w2.jpg",
     //   cobalah lengkapi sesuai dengan kebutuhan di desain
     country: "Australia",
     accomodation: "Hotel 4 Nights",
     transportation: "Qatar Airways",
-    eat: "Included as Itenerary",
+    eat: "Included as Itinerary",
     duration: {
       days: 6,
       nights: 4,
     },
-    date: "26 August 2020"
+    date: "26 August 2020",
+    quota: 12,
+    price: 148776000,
   },
   {
     id: 1,
-    title: "6D/4N Fun Tassie Vacation + Sydney",
+    title: "8D/6N Wonderful Autumn Tokyo + Kyoto",
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
     image:
-      "https://res.cloudinary.com/dmegegwqb/image/upload/v1638670749/trip-default-dewetour/sydney_p4d1w2.jpg",
+      "https://hips.hearstapps.com/hmg-prod/images/high-angle-view-of-tokyo-skyline-at-dusk-japan-royalty-free-image-1664309926.jpg",
     //   cobalah lengkapi sesuai dengan kebutuhan di desain
-    country: "Australia",
-    accomodation: "Hotel 4 Nights",
-    transportation: "Qatar Airways",
-    eat: "Included as Itenerary",
+    country: "Japan",
+    accomodation: "Hotel Tokyo",
+    transportation: "Air Japan",
+    eat: "Included as Itinerary",
     duration: {
-      days: 6,
-      nights: 4,
+      days: 8,
+      nights: 6,
     },
-    date: "26 August 2020"
+    date: "26 August 2020",
+    quota:10,
+    price: 289990000,
   }
 ];
 
@@ -87,7 +91,17 @@ exports.addTrip = async (req,res) => {
   });
 }
 
-exports.deleteTrip = async(res,req) => {
-  
+exports.deleteTrip = async(req,res) => {
+  const { id } = req.params;
+  console.log(id)
+
+  const findIndex = TRIPS.findIndex(items => items.id == id )
+  TRIPS.splice(findIndex, 1)
+  console.log(TRIPS)
+
+  res.json({
+    message: "data by id berhasil dihapus",
+    data: TRIPS
+})
 }
 // lanjutkan untuk add dan delete
